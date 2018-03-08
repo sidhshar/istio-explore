1bcbaca08543c5292d97813a09292edd9fa7616b
Sidhartha Sharma <sidhshar@cisco.com>
2018-03-08 16:38:55 +0530

    reviews deploy

---
+++ scripts/deploy_reviews.sh
@@ -0,0 +1,26 @@
+
+
+cd /home/sidhshar/repo/istio-explore/employee_v1/src/reviews
+docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
+cd reviews-wlpcfg
+docker build -t reviews-v1 --build-arg service_version=v1 .
+docker tag reviews-v1 sidhshar/examples-bookinfo-reviews-v1
+docker push sidhshar/examples-bookinfo-reviews-v1
+
+
+cd /home/sidhshar/repo/istio/samples/bookinfo/src/reviews
+docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
+cd reviews-wlpcfg
+docker build -t reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true .
+docker tag reviews-v2 sidhshar/examples-bookinfo-reviews-v2
+docker push sidhshar/examples-bookinfo-reviews-v2
+
+
+cd /home/sidhshar/repo/istio/samples/bookinfo/src/reviews
+docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
+cd reviews-wlpcfg
+docker build -t reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=red .
+docker tag reviews-v3 sidhshar/examples-bookinfo-reviews-v3
+docker push sidhshar/examples-bookinfo-reviews-v3
+
+

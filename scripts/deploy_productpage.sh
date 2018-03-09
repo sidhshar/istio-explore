6791e4e6dd3220ba57515a35703a83de8f0fcdbe
Sidhartha Sharma <sidhshar@cisco.com>
2018-03-09 11:48:08 +0530

    minor version changein scripts

--- scripts/deploy_productpage.sh
+++ scripts/deploy_productpage.sh
@@ -1,6 +1,6 @@
 
 
-cd /home/sidhshar/repo/istio-explore/employee_v2/src/productpage
+cd /home/sidhshar/repo/istio-explore/employee_v1/src/productpage
 docker build -t productpage .
 docker tag productpage sidhshar/examples-bookinfo-productpage-v1
 docker push sidhshar/examples-bookinfo-productpage-v1
--- scripts/deploy_reviews.sh
+++ scripts/deploy_reviews.sh
@@ -1,6 +1,6 @@
 
 
-cd /home/sidhshar/repo/istio-explore/employee_v2/src/reviews
+cd /home/sidhshar/repo/istio-explore/employee_v1/src/reviews
 docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
 cd reviews-wlpcfg
 docker build -t reviews-v1 --build-arg service_version=v1 .
@@ -8,7 +8,7 @@ docker tag reviews-v1 sidhshar/examples-bookinfo-reviews-v1
 docker push sidhshar/examples-bookinfo-reviews-v1
 
 
-cd /home/sidhshar/repo//istio-explore/employee_v2/src/reviews
+cd /home/sidhshar/repo//istio-explore/employee_v1/src/reviews
 docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
 cd reviews-wlpcfg
 docker build -t reviews-v2 --build-arg service_version=v2 --build-arg enable_ratings=true .
@@ -16,7 +16,7 @@ docker tag reviews-v2 sidhshar/examples-bookinfo-reviews-v2
 docker push sidhshar/examples-bookinfo-reviews-v2
 
 
-cd /home/sidhshar/repo//istio-explore/employee_v2/src/reviews
+cd /home/sidhshar/repo//istio-explore/employee_v1/src/reviews
 docker run --rm -v `pwd`:/usr/bin/app:rw niaquinto/gradle clean build
 cd reviews-wlpcfg
 docker build -t reviews-v3 --build-arg service_version=v3 --build-arg enable_ratings=true --build-arg star_color=yellow .

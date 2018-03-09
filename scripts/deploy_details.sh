2dd891c612a710e42cdc41e10f0bd64ccc894b20
Sidhartha Sharma <sidhshar@cisco.com>
2018-03-09 10:46:41 +0530

    employee explore v2

--- scripts/deploy_details.sh
+++ scripts/deploy_details.sh
@@ -1,6 +1,6 @@
 
 
-cd /home/sidhshar/repo/istio-explore/employee_v1/src/details
+cd /home/sidhshar/repo/istio-explore/employee_v2/src/details
 docker build -t details .
 docker tag details sidhshar/examples-bookinfo-details-v1
 docker push sidhshar/examples-bookinfo-details-v1
--- scripts/deploy_ratings.sh
+++ scripts/deploy_ratings.sh
@@ -1,6 +1,6 @@
 
 
-cd /home/sidhshar/repo/istio-explore/employee_v1/src/ratings
+cd /home/sidhshar/repo/istio-explore/employee_v2/src/ratings
 docker build -t ratings .
 docker tag ratings sidhshar/examples-bookinfo-ratings-v1
 docker push sidhshar/examples-bookinfo-ratings-v1

a442bfccd15228f4af5ab3ebd91dd4d3765694c2
Sidhartha Sharma <sidhshar@cisco.com>
2018-03-16 16:01:01 +0530

    add finance deploy to script

--- scripts/deploy_all.sh
+++ scripts/deploy_all.sh
@@ -2,6 +2,7 @@
 
 sh deploy_productpage.sh
 sh deploy_details.sh
+sh deploy_financedetails.sh
 sh deploy_ratings.sh
 sh deploy_reviews.sh
 
--- scripts/deploy_financedetails.sh
+++ scripts/deploy_financedetails.sh
@@ -2,7 +2,7 @@
 
 BASEPATH="/home/sidhshar/repo/istio-explore/employee_v5/src"
 
-cd $BASEPATH/details
+cd $BASEPATH/financedetails
 
 docker build -t financedetails .
 docker tag financedetails sidhshar/examples-bookinfo-financedetails-v1

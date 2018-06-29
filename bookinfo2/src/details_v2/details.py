cf66e2d251315e7801e48c9e57407d6924a55985
Sidhartha Sharma <sidhshar@cisco.com>
2018-06-29 14:50:04 +0530

    add bookinfo2

---
+++ bookinfo2/src/details_v2/details.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+#
+# Copyright 2017 Istio Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
+from flask import Flask, request, render_template, redirect, url_for
+import simplejson as json
+import requests
+import sys
+import logging
+import requests
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+app = Flask(__name__)
+logging.basicConfig(filename='microservice.log',filemode='w',level=logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+
+from flask_bootstrap import Bootstrap
+Bootstrap(app)
+
+
+@app.route('/health')
+def health():
+    return json.dumps(
+            [{
+                'status': 'Details is healthy'
+            }]
+        ), 200, {'Content-Type': 'application/json'}
+
+@app.route('/details')
+def get_book_details():
+    return json.dumps(
+            [{
+                'id' : 123,
+                'author': 'William YesShakespeare',
+                'year': 1595,
+                'type' : 'paperback',
+                'pages' : 200,
+                'publisher' : 'PublisherA',
+                'language' : 'English',
+                'ISBN-10' : '1234567890',
+                'ISBN-13' : '123-1234567890'
+            }]
+        ), 200, {'Content-Type': 'application/json'}
+
+
+@app.route('/details/<book_id>')
+def get_book_details_with_bookid(book_id):
+    return json.dumps(
+            [{
+                'id' : 456,
+                'author': 'William NoShakespeare',
+                'year': 1595,
+                'type' : 'paperback',
+                'pages' : 200,
+                'publisher' : 'PublisherB',
+                'language' : 'French',
+                'ISBN-10' : '1234567890',
+                'ISBN-13' : '123-1234567890'
+            }]
+        ), 200, {'Content-Type': 'application/json'}
+
+
+class Writer(object):
+    def __init__(self, filename):
+        self.file = open(filename,'w')
+
+    def write(self, data):
+        self.file.write(data)
+        self.file.flush()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "usage: %s port" % (sys.argv[0])
+        sys.exit(-1)
+
+    p = int(sys.argv[1])
+    sys.stderr = Writer('stderr.log')
+    sys.stdout = Writer('stdout.log')
+    print "start at port %s" % (p)
+    app.run(host='0.0.0.0', port=p, debug=True, threaded=True)
+
